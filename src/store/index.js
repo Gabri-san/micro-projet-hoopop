e77e51342f02e7d93719b0a3e6be1481cf30f124
unknown <gabriel.duchemin@laposte.net>
2022-05-02 20:59:39 +0200

    added products catalogue

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,13 @@
 import { createStore } from 'vuex'
+import productsData from '../assets/products.json'
 
 export default createStore({
-  state: {
+  state() {
+    return {
+      catalogue: productsData,
+      panier: [],
+      test: 'coucou'
+    }
   },
   getters: {
   },
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>{{this.$store.state.test}}</p>
+    <table class="table">
+      <thead>
+          <tr>
+              <th>Image</th>
+              <th>Nom</th>
+              <th>Prix</th>
+          </tr>
+      </thead>
+      <tbody>
+          <tr v-for="(user, index) in list" :key="index">
+              <td>{{index+1}}</td>
+              <td>{{user.name.first}} {{user.name.last}}</td>
+              <td>{{user.email}}</td>
+              <td>{{user.location.city}}</td>
+          </tr>
+          <tr v-for="(product, index) in this.$store.state.catalogue" :key="index">
+            <td>image</td>
+            <td>{{product.productName}}</td>
+            <td>{{product.productPrice}}</td>
+          </tr>
+      </tbody>
+  </table>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+//import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
